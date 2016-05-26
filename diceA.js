995c3be894f3ff7cd94d97f76ab374851ddd3380
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-05-26 13:35:56 -0400

    Updated RotateX with cubeFace array. Used If/else to repeat through array

--- diceA.js
+++ diceA.js
@@ -1,16 +1,22 @@
 // var cube = $(".scene .cube");
 // var animate = TweenMax.to(cube, 3, {opacity:.5;});
-var i = 0;
+var cubeFace = [
+  "translateX(200px)",
+  "rotateX(90deg)",
+  "rotateX(180deg)",
+  "rotateX(270deg)"];
 var buttonY = document.getElementById("StartB");
 var buttonX = document.getElementById("StartC");
 var Cube = document.querySelectorAll(".scene .cube");
+var i = 0;
 
 var RotateX = function(){
-  TweenLite.to(Cube, .2, {transform: "rotateX("+i+"deg)"});
-  i+=90;}
+  TweenMax.to (Cube,.5, {transform: cubeFace[i], ease:Linear.easeNone});
+  if (i===3){i===0;}else{i++}
+}
 
 var RotateY = function (){
-  TweenLite.to(Cube, .2, {transform: "rotateY("+i+"deg)"});
+  TweenMax.to(Cube, .5, {transform: "rotateY("+i+"deg)"});
   i+=90;}
 
 buttonX.onclick = RotateX;
