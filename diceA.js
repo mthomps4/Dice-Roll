1fc1bb418b713d29da5e6d62e2c70e082a79e54e
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-05-27 13:32:47 -0400

    Updated .scene .cube css now handled by Tween in javascript. Experimented more with Timeline settings for animation function.

--- diceA.js
+++ diceA.js
@@ -10,8 +10,11 @@ var cubeFace = [
 var startAnimation = document.getElementById("StartA"); //Start Animation Button
 var buttonY = document.getElementById("StartB"); //Rotate Y Button (Still Rotates around X?)
 var buttonX = document.getElementById("StartC"); // Rotate X Button
-var Cube = document.querySelectorAll(".scene .cube");
+var Cube = document.querySelectorAll(".cube");
+var Scene = document.querySelectorAll(".scene");
 var i = 0;
+
+TweenMax.set(Cube, {perspective:0});
 
 //Rotates through cubeFace array on each click.
 //If i>cubeFace reset to 0 and preform 1st step.
@@ -31,16 +34,25 @@ var RotateX = function (){
   i+=90;}
 
 //Place this in function to control time to button specific action
-var tl = new TimelineMax();
+var tl = new TimelineMax({paused:true, repeat:5});
+    tl.yoyo(true);
+    tl.to(Cube, 1, {rotation:360})
+    // .to(Scene, 1, {scale:.5}, "-=1")
+    // .to(Scene, 1, {scale:.1},  "-=1")
+    // .to(Cube, 1, {rotationX:90}, "-=1")
+    // .to(Scene, 1, {scale:1.3}, "-=.5")
+    // .to(Cube, 1, {rotationY:90}, "-=1")
+    // .to(Cube, 1, {rotation:0}, "-=1")
+    .to(Cube, 1, {rotationY:360, rotationX:360}, "-=1")
+    .to(Scene,1, {scale:.2},"-=1")
+    .to(Cube, 1, {x:500}, "-=1")
+    // .to(Scene, 1, {scale:1}, "-=.5")
 
-    tl.from(Cube, 1, {rotationX:0})
-    .to(Cube, 1, {rotationX:90})
-    .to(Cube, 1, {rotationX:180})
-    .to(Cube, 1, {rotationX:270})
-    .to(Cube, 1, {rotationX:360});
+
+    // .to(Cube, 1, {rotation:0}, "-=1");
 
     // tl.totalDuration(2);
-    tl.timeScale(6);
+    tl.timeScale(1);
 
 
 var start = function(){tl.restart();};
